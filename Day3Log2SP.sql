41e6b6ca814ab9d3dded5ddbd3acb9baf6a1cf45
parekhsaloni7 <39058580+parekhsaloni7@users.noreply.github.com>
2018-05-09 11:39:04 -0400

    Add files via upload

---
+++ Day3Log2SP.sql
@@ -0,0 +1,14 @@
+MariaDB [CSD2204S18]> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| csd2204s18         |
+| information_schema |
+| mysql              |
+| performance_schema |
+| phpmyadmin         |
+| test               |
++--------------------+
+6 rows in set (0.00 sec)
+
+MariaDB [CSD2204S18]> EXIT;
